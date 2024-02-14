d2bcf3587c4b7df4b33783d8400d703c107f41e7
invtfl-bharti <bhartijayprakash19@gmail.com>
2024-02-14 23:37:57 +0530

    js file added

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -9,8 +9,15 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
        
     }
 
     inputBox.value = "";
 }
+
+
+listContainer.addEventListener("click", function (s) {
+});
