260707e711d17f1d29c69fcb24454eea05aa501d
invtfl-bharti <bhartijayprakash19@gmail.com>
2024-05-05 22:27:54 +0530

    add files

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,35 +1,52 @@
-let btn = document.querySelector('.btn');
-
 let main2 = document.querySelector('.main2');
+let btn = document.querySelector('.btn');
+let length = document.querySelector('#length');
+let passwordDisplay = document.querySelector('[data-lengthSlider]');
+let lettersCheck = document.querySelector('.p2');
+let numbersCheck = document.querySelector('.p1');
+let symbolsCheck = document.querySelector('.p3');
+let indicator = document.querySelector('.strengthValue');
+let generateButton = docment.querySelector('.btn2');
+let allSliderButton = document.querySelector('p');
+const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
+
+
+
+
+
+
+
+
+
 btn.addEventListener('click', function () {
-main2.scrollIntoView({ behavior: 'smooth' });
-})
-
-
+    main2.style.display = "block";
+    main2.scrollIntoView({ behavior: 'smooth' });
+});
 let skip = document.querySelector('.skip');
-
 let main = document.querySelector('.main');
 skip.addEventListener('click', function () {
-main.scrollIntoView({ behavior: 'smooth' });
-})
-
+    main.scrollIntoView({ behavior: 'smooth' });
+});
 let copyicon = document.querySelector('.bx-copy');
 
 copyicon.addEventListener('click', function () {
-    var copyText = document.getElementById('copyText');
+    // Assuming there's an input field with the ID 'password'
+    var copyText = document.getElementById('password');
     copyText.select();
     document.execCommand('copy');
     alert('Copied to clipboard: ' + copyText.value);
 });
 
-// slider using js
+// Slider using JavaScript
 const slider = document.querySelector('[data-lengthSlider]');
 const inputValue = document.querySelector('#inputValue');
-let passwordLength = 0;
-
 
 slider.addEventListener("input", (event) => {
     passwordLength = event.target.value;
-
     inputValue.innerText = passwordLength;
-})
+});
+
+let password = "";
+let passwordLength = 10;
+let checkCount = 0;
