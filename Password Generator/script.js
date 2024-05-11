696548ca30c8795982489b576cb8d561ce88069c
invtfl-bharti <bhartijayprakash19@gmail.com>
2024-05-11 23:58:20 +0530

    files added

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,18 +1,19 @@
 let main2 = document.querySelector('.main2');
 let btn = document.querySelector('.btn');
 let copyicon = document.querySelector('.bx-copy');
-let length = document.querySelector('#length');
+let sliderValue = document.querySelector('#sliderValue');
 let passwordgen = document.querySelector('#Password');
-let passwordDisplay = document.querySelector('[data-lengthSlider]');
+let inputSlider = document.querySelector('[data-lengthSlider]');
 let skip = document.querySelector('.skip');
 let main = document.querySelector('.main');
-let lettersCheck = document.querySelector('.p2');
+let uppercaseCheck = document.querySelector('.p2');
 let numbersCheck = document.querySelector('.p1');
-let symbolsCheck = document.querySelector('.p3');
-let indicator = document.querySelector('.strengthValue');
+let lowercaseCheck = document.querySelector('.p3');
+let symbolsCheck = document.querySelector('.p4');
+let indicator = document.querySelector('.bx-circle');
 let generateButton = document.querySelector('.btn2');
 let allSliderButton = document.querySelector('p');
-const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+let genPass = document.querySelector('.btn2');
 
 
 
@@ -46,8 +47,35 @@ skip.addEventListener('click', function () {
 // Slider using JavaScript
 const slider = document.querySelector('[data-lengthSlider]');
 // Corrected selector for ID
-slider.addEventListener("input", (event) => {
-    const passwordLength = event.target.value; // Declare passwordLength using const
-    length.innerText = passwordLength; // Update the text content of the <p> element
+// slider.addEventListener("input", (event) => {
+//     const passwordLength = event.target.value; // Declare passwordLength using const
+    // length.innerText = passwordLength; // Update the text content of the <p> element
+// });
+inputSlider.value = 0;
+
+inputSlider.oninput = (() => {
+    let value = inputSlider.value;
+    sliderValue.textContent = value;
+});
+let allNumber = "0123456789";
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let symbols = " ~`!@#$%^&*()_-+={[}]|:;<,>.?/";
+//  Function to generate password
+
+genPass.addEventListener('click', () => {
+    // Call the generatePassword function and assign the generated password to the passwordgen element
+    passwordgen.value = generatePassword(inputSlider.value);
 });
 
+function generatePassword() {
+    let genPassword = "";
+    let allChars = "";
+    allChars += lowercaseCheck.checked ? lowercase : "";
+    allChars += uppercaseCheck.checked ? uppercase : "";
+    allChars += numbersCheck.checked ? allNumber : "";
+    allChars += symbolsCheck.checked ? symbols : "";
+
+    genPassword = uppercase.charAt(Math.floor(Math.random() * uppercase.length ));
+    return genPassword;
+}
